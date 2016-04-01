56ce5a2c019913f36df86b0f76c6db24a235c15e
Jack Phoenix <jack@countervandalism.net>
2016-04-01 15:47:45 +0300

    Version 2.1: extension registration support added
    Change-Id: I4b6759a9567edfb6b34239d2283cd69dafd2daca

--- InviteFriendOnEdit.php
+++ InviteFriendOnEdit.php
@@ -1,128 +1,136 @@
 <?php
 
-$wgHooks['PageContentSaveComplete'][] = 'wfInviteFriendToEdit';
-$wgHooks['PageContentInsertComplete'][] = 'wfCreateOpinionCheck';
-$wgHooks['OutputPageBeforeHTML'][] = 'wfInviteRedirect';
-$wgHooks['OutputPageBeforeHTML'][] = 'wfDisplayInviteLinks';
+class MiniInviteHooks {
 
-function wfInviteFriendToEdit( $article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId ) {
-	if ( !( $flags & EDIT_NEW ) ) {
-		// Increment edits for this page by one (for this user's session)
-		$edits_views = ( isset( $_SESSION['edits_views'] ) ? $_SESSION['edits_views'] : array( $article->getID() => 0 ) );
-		$page_edits_views = $edits_views[$article->getID()];
-		$edits_views[$article->getID()] = ( $page_edits_views + 1 );
-
-		$_SESSION['edits_views'] = $edits_views;
+	// Sets the default value of $wgEmailFrom since that cannot be done in
+	// extension.json, obviously, because JSON is not PHP
+	public static function registerExtension() {
+		global $wgEmailFrom, $wgPasswordSender;
+		// The email address where invite emails are sent out from
+		$wgEmailFrom = $wgPasswordSender;
 	}
-	return true;
-}
 
-function wfCreateOpinionCheck( $wikiPage, User $user, $content, $summary, $isMinor, $isWatch, $section, $flags, Revision $revision ) {
-	global $wgSendNewArticleToFriends;
+	public static function inviteFriendToEdit( $article, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId ) {
+		if ( !( $flags & EDIT_NEW ) ) {
+			// Increment edits for this page by one (for this user's session)
+			$edits_views = ( isset( $_SESSION['edits_views'] ) ? $_SESSION['edits_views'] : array( $article->getID() => 0 ) );
+			$page_edits_views = $edits_views[$article->getID()];
+			$edits_views[$article->getID()] = ( $page_edits_views + 1 );
 
-	if ( $wgSendNewArticleToFriends ) {
-		global $wgLang;
+			$_SESSION['edits_views'] = $edits_views;
+		}
+		return true;
+	}
 
-		$title = $wikiPage->getTitle();
-		// If the user has created a new opinion, we want to turn on a session flag
-		$dbr = wfGetDB( DB_MASTER );
-		$res = $dbr->select(
-			'categorylinks',
-			array( 'cl_to' ),
-			array( 'cl_from' => $title->getArticleID() ),
-			__METHOD__
-		);
+	public static function createOpinionCheck( $wikiPage, User $user, $content, $summary, $isMinor, $isWatch, $section, $flags, Revision $revision ) {
+		global $wgSendNewArticleToFriends;
 
-		foreach ( $res as $row ) {
-			// @todo FIXME: this is way too site-specific...
-			if ( $wgLang->uc( $row->cl_to ) == 'OPINIONS' ) {
-				$_SESSION['new_opinion'] = $title->getText();
+		if ( $wgSendNewArticleToFriends ) {
+			global $wgLang;
+
+			$title = $wikiPage->getTitle();
+			// If the user has created a new opinion, we want to turn on a session flag
+			$dbr = wfGetDB( DB_MASTER );
+			$res = $dbr->select(
+				'categorylinks',
+				array( 'cl_to' ),
+				array( 'cl_from' => $title->getArticleID() ),
+				__METHOD__
+			);
+
+			foreach ( $res as $row ) {
+				// @todo FIXME: this is way too site-specific...
+				if ( $wgLang->uc( $row->cl_to ) == 'OPINIONS' ) {
+					$_SESSION['new_opinion'] = $title->getText();
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public static function inviteRedirect( OutputPage &$out, &$text ) {
+		global $wgSendNewArticleToFriends;
+		if ( $wgSendNewArticleToFriends ) {
+			if ( isset( $_SESSION['new_opinion'] ) ) {
+				$invite = SpecialPage::getTitleFor( 'EmailNewArticle' );
+				$out->redirect( $invite->getFullURL( array( 'page' => $_SESSION['new_opinion'] ) ) );
+				unset( $_SESSION['new_opinion'] );
 			}
 		}
 	}
-	return true;
-}
 
-function wfInviteRedirect( OutputPage &$out, &$text ) {
-	global $wgSendNewArticleToFriends;
-	if ( $wgSendNewArticleToFriends ) {
-		if ( isset( $_SESSION['new_opinion'] ) ) {
-			$invite = SpecialPage::getTitleFor( 'EmailNewArticle' );
-			$out->redirect( $invite->getFullURL( array( 'page' => $_SESSION['new_opinion'] ) ) );
-			unset( $_SESSION['new_opinion'] );
+	public static function displayInviteLinks( OutputPage &$out, &$text ) {
+		// We need a WikiPage in order to get the page ID
+		if ( !$out->canUseWikiPage() ) {
+			return true;
 		}
-	}
-}
 
-function wfDisplayInviteLinks( OutputPage &$out, &$text ) {
-	// We need a WikiPage in order to get the page ID
-	if ( !$out->canUseWikiPage() ) {
+		if ( !isset( $_SESSION['edits_views'] ) ) {
+			// To avoid "undefined <whatever variable/offset/etc.>" bullshit
+			return true;
+		}
+
+		$t = $out->getTitle();
+		$user = $out->getUser();
+		$wikiPage = $out->getWikiPage();
+		$s = ''; // the stuff that should be shown to the end-user
+
+		if (
+			!$out->isArticle() || $t->isMainPage() || $t->isTalkPage() ||
+			$t->inNamespaces( NS_SPECIAL, NS_MEDIAWIKI ) ||
+			$t->equals( Title::makeTitleSafe( NS_USER, $t->getText() ) )
+		)
+		{
+			return true;
+		}
+
+		$edits_views = $_SESSION['edits_views'];
+		// page ID is not set when creating a new page (obviously), so using $wikiPage->getID()
+		// directly as-is can result in an E_NOTICE about undefined offsets on the
+		// $page_edits_views variable definition line below
+		$pageId = ( $wikiPage->getID() !== null ) ? $wikiPage->getID() : 0;
+		$page_edits_views = $edits_views[$pageId];
+
+		if ( $page_edits_views == 1 && $user->isLoggedIn() ) {
+			$invite_title = SpecialPage::getTitleFor( 'InviteEmail' );
+			$s .= '<span id="invite_to_edit" class="edit" style="display:none; background-color:#FFFB9B">';
+			$s .= Linker::link(
+				$invite_title,
+				wfMessage( 'invite-friend-to-edit' )->plain(),
+				array(),
+				array( 'email_type' => 'edit', 'page' => $t->getText() )
+			);
+			$s .= '</span>';
+			$edits_views[$wikiPage->getID()] = $page_edits_views + 1;
+			$_SESSION['edits_views'] = $edits_views;
+		}
+
+		// This was originally commented out, but I have no idea why...
+		// Oh, maybe it conflicts with wfInviteRedirect()? Not sure, @todo CHECKME
+		if ( isset( $_SESSION['new_opinion'] ) && $_SESSION['new_opinion'] == 1 ) {
+			$invite_title = SpecialPage::getTitleFor( 'InviteEmail' );
+			$s .= '<span id="invite_to_read" class="edit" style="display:none; background-color:#FFFB9B">';
+			$s .= Linker::link(
+				$invite_title,
+				wfMessage( 'invite-friend-to-read' )->plain(),
+				array(),
+				array( 'email_type' => 'view', 'page' => $t->getText() )
+			);
+			$s .= '</span>';
+			$_SESSION['new_opinion'] = 0;
+		}
+
+		if ( !empty( $s ) ) {
+			$out->addModules( 'ext.miniInvite.DisplayInviteLinks.js' );
+			// Output the HTML. addHTML() places it at the very beginning of the
+			// page, which is where we want it; appending to $text places it at the
+			// very *bottom* of the page, which is what we do *not* want.
+			$out->addHTML( $s );
+			#$text .= $s;
+		}
+
 		return true;
 	}
 
-	if ( !isset( $_SESSION['edits_views'] ) ) {
-		// To avoid "undefined <whatever variable/offset/etc.>" bullshit
-		return true;
-	}
-
-	$t = $out->getTitle();
-	$user = $out->getUser();
-	$wikiPage = $out->getWikiPage();
-	$s = ''; // the stuff that should be shown to the end-user
-
-	if (
-		!$out->isArticle() || $t->isMainPage() || $t->isTalkPage() ||
-		$t->inNamespaces( NS_SPECIAL, NS_MEDIAWIKI ) ||
-		$t->equals( Title::makeTitleSafe( NS_USER, $t->getText() ) )
-	)
-	{
-		return true;
-	}
-
-	$edits_views = $_SESSION['edits_views'];
-	// page ID is not set when creating a new page (obviously), so using $wikiPage->getID()
-	// directly as-is can result in an E_NOTICE about undefined offsets on the
-	// $page_edits_views variable definition line below
-	$pageId = ( $wikiPage->getID() !== null ) ? $wikiPage->getID() : 0;
-	$page_edits_views = $edits_views[$pageId];
-
-	if ( $page_edits_views == 1 && $user->isLoggedIn() ) {
-		$invite_title = SpecialPage::getTitleFor( 'InviteEmail' );
-		$s .= '<span id="invite_to_edit" class="edit" style="display:none; background-color:#FFFB9B">';
-		$s .= Linker::link(
-			$invite_title,
-			wfMessage( 'invite-friend-to-edit' )->plain(),
-			array(),
-			array( 'email_type' => 'edit', 'page' => $t->getText() )
-		);
-		$s .= '</span>';
-		$edits_views[$wikiPage->getID()] = $page_edits_views + 1;
-		$_SESSION['edits_views'] = $edits_views;
-	}
-
-	// This was originally commented out, but I have no idea why...
-	// Oh, maybe it conflicts with wfInviteRedirect()? Not sure, @todo CHECKME
-	if ( isset( $_SESSION['new_opinion'] ) && $_SESSION['new_opinion'] == 1 ) {
-		$invite_title = SpecialPage::getTitleFor( 'InviteEmail' );
-		$s .= '<span id="invite_to_read" class="edit" style="display:none; background-color:#FFFB9B">';
-		$s .= Linker::link(
-			$invite_title,
-			wfMessage( 'invite-friend-to-read' )->plain(),
-			array(),
-			array( 'email_type' => 'view', 'page' => $t->getText() )
-		);
-		$s .= '</span>';
-		$_SESSION['new_opinion'] = 0;
-	}
-
-	if ( !empty( $s ) ) {
-		$out->addModules( 'ext.miniInvite.DisplayInviteLinks.js' );
-		// Output the HTML. addHTML() places it at the very beginning of the
-		// page, which is where we want it; appending to $text places it at the
-		// very *bottom* of the page, which is what we do *not* want.
-		$out->addHTML( $s );
-		#$text .= $s;
-	}
-
-	return true;
 }
--- MiniInvite.php
+++ MiniInvite.php
@@ -10,62 +10,20 @@
  *
  * @file
  * @ingroup Extensions
- * @version 2.0
  * @author Aaron Wright <aaron.wright@gmail.com>
  * @author David Pean <david.pean@gmail.com>
  * @author Jack Phoenix <jack@countervandalism.net>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-// Extension credits that show up on Special:Version
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'MiniInvite',
-	'version' => '2.0',
-	'author' => array( 'Aaron Wright', 'David Pean', 'Jack Phoenix' ),
-	'description' => '[[Special:InviteEmail|A special page to invite your friends to join the wiki]]',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:MiniInvite',
-);
-
-// Internationalization
-$wgMessagesDirs['MiniInvite'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['MiniInviteAliases'] = __DIR__ . '/MiniInvite.alias.php';
-
-// Set up the new special pages
-$wgAutoloadClasses['InviteEmail'] = __DIR__ . '/SpecialInviteEmail.php';
-$wgSpecialPages['InviteEmail'] = 'InviteEmail';
-$wgAutoloadClasses['EmailNewArticle'] = __DIR__ . '/SpecialEmailNewArticle.php';
-$wgSpecialPages['EmailNewArticle'] = 'EmailNewArticle';
-
-// Load the hooked functions
-require_once 'InviteFriendOnEdit.php';
-
-// ResourceLoader support for MediaWiki 1.17+
-$wgResourceModules['ext.miniInvite.css'] = array(
-	'styles' => 'resources/css/invite.css',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MiniInvite',
-	'position' => 'top'
-);
-
-$wgResourceModules['ext.miniInvite.emailNewArticle.css'] = array(
-	'styles' => 'resources/css/EmailNewArticle.css',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MiniInvite',
-	'position' => 'top'
-);
-
-$wgResourceModules['ext.miniInvite.DisplayInviteLinks.js'] = array(
-	'scripts' => 'resources/js/DisplayInviteLinks.js',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'MiniInvite'
-);
-
-# Configuration settings
-// The email address where invite emails are sent out from
-$wgEmailFrom = $wgPasswordSender;
-
-// When set to true, after a user has created a page in the blog category (see
-// [[mw:Extension:BlogPage]]), they are redirected to Special:EmailNewArticle,
-// prompting them to email their friends about the newly-created blog article.
-$wgSendNewArticleToFriends = false;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'MiniInvite' );
+	$wgMessagesDirs['MiniInvite'] =  __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for MiniInvite extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the MiniInvite extension requires MediaWiki 1.25+' );
+}
